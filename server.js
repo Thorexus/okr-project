8753adb86bbab46003feb336b7d30ee1d500d84d
watcharaphan <Watcharaphan_p@cmu.ac.th>
2020-10-16 15:20:05 +0700

    test

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader("Content-Type", "text/plain")
-  res.end("Hello World 123")
+  res.end("Hello World 1234")
 })
 
 server.listen(port, hostname, () => {
